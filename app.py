b551e8d8a74923141144d2fa15b20458d97f786d
CODED CLOUDS <hashimsaqib46@gmail.com>
2026-05-18 15:44:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_talisman import Talisman
 from flask_cors import CORS
+import certifi
 load_dotenv()
 
 app = Flask(__name__)
